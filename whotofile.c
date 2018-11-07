6260ddb7939de70421e0a4ea1ec192074c9b02e9
kahetellet <kahetellt@gmail.com>
2018-11-07 12:34:23 +0900

    6

---
+++ whotofile.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+void main()
+{
+	int pid;
+	int fd;
+
+	printf("About to run who into a file\n");
+	if (pid = fork() == -1)
+	{
+		perror("fork"); exit(1);
+	}
+	if (pid == 0)
+	{
+		close(1);
+		fd = creat("userlist", 0664);
+		execlp("who", "who", NULL);
+		perror("execlp");
+		exit(1);
+	}
+
+	if (pid != 0)
+	{
+		wait(NULL);
+		printf("Done running who. results in userlist\n");
+	}
+}
